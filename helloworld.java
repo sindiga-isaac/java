a0a526f6cf9e27c81d0b3ea9a2530b2f513c901b
isaac <59864350+sindiga1@users.noreply.github.com>
2021-10-05 09:05:38 +0300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
 public class Main{
+    //This is the main memthod
     public static void main(strings[]args){
+        //This is a  code statement
         System.out.println("Hello World");
     }    
 }        
